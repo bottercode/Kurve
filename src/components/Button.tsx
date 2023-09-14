61e9923e48f7abcf70a504e56d6ea024fff22f57
devansh dwivedi <devanshdwivedi68@gmail.com>
2023-09-14 18:03:50 +0530

    init

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,16 @@ import React, { useState } from "react";
 
 function Button() {
   const [isClicked, setIsClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = () => {
     setIsClicked(true);
+    setIsLoading(true);
+
+    // Simulate a loading delay (you can replace this with actual data loading)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the delay as needed
   };
 
   const handleMouseLeave = () => {
@@ -13,14 +20,22 @@ function Button() {
   };
 
   return (
-    <div
-      className={`border-pink-500 border mt-10 ml-10 w-28 h-8 shadow-[5px_5px_0px_0px_rgba(109,40,217)]  transform ${
-        isClicked ? "scale-95" : "scale-100"
-      } transition-transform duration-150 ease-in-out`}
-      onClick={handleClick}
-      onMouseLeave={handleMouseLeave}
-    >
-      <p className="pl-5 pt-2 text-xs">SHOP NOW</p>
+    <div>
+      <button
+        className={`border-pink-500 border mt-10 ml-10 w-28 h-8 shadow-[5px_5px_0px_0px_rgba(109,40,217)]  transform ${
+          isClicked ? "scale-95" : "scale-100"
+        } transition-transform duration-150 ease-in-out`}
+        onClick={isLoading ? undefined : handleClick}
+        onMouseLeave={handleMouseLeave}
+        disabled={isLoading}
+      >
+        <p className="p-2 text-xs">SHOP NOW</p>
+      </button>
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-70 z-50 transition-opacity duration-1000">
+          <p className="text-black text-lg">Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import chair from "../assets/suchit-poojari-ljRiZl00n18-unsplash.jpg";
+import chair from "../assets/chair.png";
 import Image from "next/image";
 import Button from "./Button";
+import { ArrowRight } from "lucide-react";
+import { Github } from "lucide-react";
+import { Twitter } from "lucide-react";
 const LandingPage = () => {
   return (
     <div>
@@ -14,13 +17,29 @@ const LandingPage = () => {
       <h2 className="ml-10 mt-24 text-5xl">Newmanaly</h2>
       <h2 className="ml-10 text-5xl"> Beautica </h2>
       <Button />
-
-      <div className="ml-10 mt-24 bg-[#83a9ac]">
-        <h1>STAY IN THE LOOP</h1>
-        <p>if you wish to receive daily updates.</p>
-        <input />
+      <div className="absolute right-[5%] top-10 z-20 font-sans">
+        <p>Designed by</p>
+        <p className="font-bold">devansh.</p>
       </div>
-      <h1 className="text-black text-9xl font-sans max-sm:text-5xl max-sm:left-[30%] absolute left-[41%] top-5 z-10">
+      <div className="absolute right-[6%] bottom-[10%]">
+        <Github className="mb-5" />
+        <Twitter />
+      </div>
+
+      <div className="ml-10 mt-64 bg-[#83a9ac] font-sans">
+        <h1>STAY IN THE LOOP</h1>
+        <p>Receive monthly updates about the product and other invites.</p>
+        <div className="flex mt-5 font-sans">
+          <input
+            placeholder="Enter email address"
+            className="bg-transparent text-black placeholder:text-black  outline-none "
+          />
+          <ArrowRight className="ml-6" />
+        </div>
+        <hr className="text-black mt-2 w-[12%]" />
+      </div>
+
+      <h1 className="text-black text-[200px] tracking-widest font-sans max-sm:text-5xl max-sm:left-[30%] absolute left-[40%] bottom-[75%]">
         KURVE
       </h1>
       <Image
@@ -33,6 +52,9 @@ const LandingPage = () => {
       />
       <p className="-rotate-90 absolute left-[39%] top-1/2 font-bold">
         the collection
+      </p>
+      <p className="absolute left-[38%] bottom-[10%] w-32 font-sans">
+        Shop now to see the full collection
       </p>
     </div>
   );
